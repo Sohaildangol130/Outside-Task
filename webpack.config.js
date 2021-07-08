5cfddfa86a23bb0a0cd6fc41813e56eac4e81b96
Sohaildangol130 <sohaildangol130@gmail.com>
2021-07-08 11:31:12 +0545

    Added typography

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,20 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
-      }
-    ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }   
+    ],
   },
   watch: true,
   mode: 'development'
