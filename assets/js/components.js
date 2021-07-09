061263e70302377150666ffba48d213601cecf9a
Sohaildangol130 <sohaildangol130@gmail.com>
2021-07-09 12:26:41 +0545

    Created header component and installed jquery

---
+++ assets/js/components.js
@@ -0,0 +1,1 @@
+$('.header').load('./components/header.html');
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,11 @@
+//importing css styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '../css/global.css';
+
+//importing jquery
+import $ from 'jquery';
+window.$ = $;
+
+//importing js partials
+require ('./components');
