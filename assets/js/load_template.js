1d39e44109f9180c4a8116f8de916262d63325a5
Sohaildangol130 <sohaildangol130@gmail.com>
2021-07-11 10:42:46 +0545

    Created main component

--- assets/js/load_template.js
+++ assets/js/load_template.js
@@ -2,3 +2,4 @@ $('.header').load('./sections/header.html');
 $('.above-the-fold').load('./sections/above_the_fold.html');
 $('.news').load('./sections/news.html');
 $('.mission').load('./sections/mission.html');
+$('.benificiaries').load('./sections/benificiaries.html');
