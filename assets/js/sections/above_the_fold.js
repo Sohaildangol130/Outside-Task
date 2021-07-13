76e4f62081c9a56c512a4ffd1944e77fddb82b9b
Sohaildangol130 <sohaildangol130@gmail.com>
2021-07-13 09:49:57 +0545

    News section animation

--- assets/js/sections/above_the_fold.js
+++ assets/js/sections/above_the_fold.js
@@ -9,5 +9,5 @@ $(()=>{
     .from(".above-the-fold__bg", {opacity: 0, y: 200, duration:1})
     .from(".main__text", {opacity: 0, duration: 1}, "-=.5")
     .from(".main__button", {opacity: 0, duration:1}, "-=1")
-    .from(".above-the-fold__image img", {opacity: 0, y: 200}, "-=.7")
+    .from(".above-the-fold__image img", {opacity: 0, scale: 0}, "-=.7")
 })
--- assets/js/sections/global.js
+++ assets/js/sections/global.js
@@ -1,2 +1,3 @@
 require('./header');
 require('./above_the_fold');
+require('./news');
